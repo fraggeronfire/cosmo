09fdb859dfcde5d444f7e93047970039abfc0219
fraggeronfire <75206374+fraggeronfire@users.noreply.github.com>
2021-09-03 16:42:07 +0300

    Delete cosmo directory

--- cosmo/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $(".slider__box").slick({
-    prevArrow:
-      '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow">',
-    nextArrow:
-      '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow">',
-  });
-});
